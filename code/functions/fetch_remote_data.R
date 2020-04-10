5b43437df9edcf92ec0e3d833d691954c69c3c22
McCrea Cobb <mccrea_cobb@fws.gov>
2020-04-10 11:40:48 -0800

    tidy fetch_remote_data.R

--- code/functions/fetch_remote_data.R
+++ code/functions/fetch_remote_data.R
@@ -35,50 +35,46 @@ fetch_remote_data <- function(flat.gps, dat.collar, clean, save, savedir, return
   dat.collar <- get(load(dat.collar))
   rm(x)
   
+  # Reformat the telemetry data
   message("Formatting data...")
-  dat.collar$id <- dat.collar$ctn
-  dat$date <- as.Date(dat$fixtime)
-  dat$month <- as.numeric(format(dat$date, "%m"))
+  
+  dat.collar$id <- dat.collar %>%
+    mutate(id = ctn) %>%
+    mutate(site = c(rep("TET", 45), rep("WIS", 11), rep("KAN", 20), rep("YKF", 18), rep("KOY", 28)))  # Add a site variable to capture data (temporary fix)
+  
+  dat$date <- dat %>%
+    mutate(date = as.Date(fixtime),
+           month = as.numeric(format(date, "%m")))
   dat <- dat[complete.cases(dat$lat), ]   # Remove NA rows from lat/lon
   
-  # Add a site variable (temporary fix)
-  dat.collar$site <- c(rep("TET", 45), rep("WIS", 11), rep("KAN", 20), rep("YKF", 18), rep("KOY", 28))
-  
-  dat <- merge(dat, dat.collar, by="id", all.x=TRUE)
+  # Merge telemetry and capture datasets:
+  dat <- merge(dat, dat.collar, by = "id", all.x = TRUE)
   rm(dat.collar)
   
   # Convert all factors to characters:
   i <- sapply(dat, is.factor)
   dat[i] <- lapply(dat[i], as.character) ; rm(i)
   
-  if (clean==TRUE) {
-    # Subset out columns not needed for the app to reduce file size:
-    dat <- dat %>% dplyr::select(-c(utmzone, utmy, utmx, alt, fixtype, hdop, 
-                                    nsats, step, angle, deploy.site))
-    
-    # Remove duplicate fixes:
-    dat <- dat %>% distinct(id, fixtime, .keep_all = T)
-    
-    # 700521A (KNI): remove fixes after it was trapped:
-    dat <- dat %>% filter(!(id == "700521A" & date > as.Date("2018-10-28")))
-    
-    # 700552A (YF): remove fixes after it died:
-    dat <- dat %>% filter(!(id == "700552A" & date > as.Date("2018-06-18")))
-    
-    # 700534A (Kanuti): remove fixes after it died (? or something):
-    dat <- dat %>% filter(!(id == "700534A" & date > as.Date("2018-05-04")))
-    
-    dat <- dat %>% arrange(ctn, fixtime)
+  if (clean == TRUE) {
+    dat <- dat %>% 
+      dplyr::select(-c(utmzone, utmy, utmx, alt, fixtype, hdop,  # Subset out columns not needed for the app to reduce file size:
+                                    nsats, step, angle, deploy.site)) %>%
+      distinct(id, fixtime, .keep_all = T) %>%  # Remove duplicate fixes
+      filter(!(id == "700521A" & date > as.Date("2018-10-28")),      # Remove fixes after 700521A (KNI) was trapped
+             id == "700552A" & date > as.Date("2018-06-18"),         # Remove fixes after 700552A (YF) died
+             !(id == "700534A" & date > as.Date("2018-05-04"))) %>%  # Remove fixes after 700534A (Kanuti) died (? or went off air..)
+      filter() %>%  
+      arrange(ctn, fixtime)
   }
   
-  if (save==TRUE) {
+  if (save == TRUE) {
     # Save it:
-    save(dat, file=savedir)
+    save(dat, file = savedir)
   }
   
   message("Done!")
   
-  if (returnit==TRUE) {
+  if (returnit == TRUE) {
     return(dat)
   }
   
